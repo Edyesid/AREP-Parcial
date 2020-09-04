e7da6d6da2ed0e4cbfa34f4d7dffa31ac65d52c7
Edyesid <edyesid2000@gmail.com>
2020-09-04 12:58:56 -0500

    segundo commit

--- src/main/java/edu/escuelaing/arep/app/App.java
+++ src/main/java/edu/escuelaing/arep/app/App.java
@@ -1,13 +1,47 @@
 package edu.escuelaing.arep.app;
 
+import static spark.Spark.*;
+
+import java.util.ArrayList;
+
 /**
- * Hello world!
+ * @author Edwin Rodriguez Clase principal de lectura de archivos y impresion de
+ *         los datos calculados
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	/**
+	 * Encargada de iniciar el servicio usando spark
+	 * @param args parametros al momento de iniciar la aplicación
+	 */
+    public static void main(String [] args){
+        staticFiles.location("/public");
+        port(getPort());
+		post("/calculator", (request, response) -> {
+			
+			ArrayList<Double> list = new ArrayList<Double>();
+	    	
+	    	list.add(5.0);
+	    	list.add(6.0);
+	    	list.add(7.0);
+	    	list.add(8.0);
+	    	list.add(9.0);
+	    	list.add(10.0);
+	    	
+	    	Calculator cal = new Calculator(list);
+			
+			return "{\"mean\":" + cal.sum() + ", \"dev\":" + cal.average() + "}";
+ 
+		});
+    } 
+    /**
+     * retorna el puerto solicitado por la aplicacion
+     * @return puerto
+     */
+	static int getPort() {
+		 if (System.getenv("PORT") != null) {
+			 return Integer.parseInt(System.getenv("PORT"));
+		 }
+		 return 4567;
+	}
 }
---
+++ src/main/java/edu/escuelaing/arep/app/Calculator.java
@@ -0,0 +1,52 @@
+package edu.escuelaing.arep.app;
+
+import java.awt.List;
+import java.util.ArrayList;
+
+public class Calculator {
+	/**
+	 * Realiza diferentes calculos a la lista que entra
+	 */
+	private ArrayList<Double> list = new ArrayList<Double>();
+	
+	public Calculator(ArrayList<Double> list) {
+		this.list = list;
+	}
+	
+	public ArrayList<Double> bubbleSort() {
+		double temp;
+		int len = list.size();
+		for (int i = 0; i < len; i++) {
+			for (int j = 0; j < len; j++) {
+				if (list.get(j) < list.get(j + 1)) {
+					temp = list.get(j);
+					list.set(j, list.get(j + 1));
+					list.set(j + 1, temp);
+				}
+			}
+		}
+		return list;
+	}
+	/**
+	 * Suma los valores de la lista
+	 * @return suma
+	 */
+	public double sum() {
+		double suma = 0;
+		for (int i = 0; i < list.size(); i++) {
+			suma += list.get(i);
+		}
+		System.out.println(Math.round(suma * 100.0) / 100.0);
+		return Math.round(suma * 100.0) / 100.0;
+	}
+	/**
+	 * saca el promedio de la lista
+	 * @return promedio
+	 */
+	public double average() {
+		double average = 0;
+		double suma = this.sum();
+		average = suma / list.size();
+		return Math.round(average * 100.0) / 100.0;
+	}
+}
